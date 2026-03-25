2cb248a7a847466f73127f8b92f885343937a313
João Victor Oliveira Cavalcante <jvocx.dev@gmail.com>
2026-03-25 11:40:08 -0300

    commit

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,64 +1,46 @@
 const url = "data/members.json";
-
 const container = document.querySelector("#members");
 
 async function getMembers() {
-
     const response = await fetch(url);
-
     const data = await response.json();
-
     displayMembers(data);
-
 }
 
 function displayMembers(members) {
+    container.innerHTML = "";
 
     members.forEach(member => {
-
         const card = document.createElement("section");
 
         card.innerHTML = `
-<img src="images/${member.image}" alt="${member.name}">
-<h3>${member.name}</h3>
-<p>${member.address}</p>
-<p>${member.phone}</p>
-<a href="${member.website}" target="_blank">Visit Website</a>
-`;
+            <img src="images/${member.image}" alt="${member.name}">
+            <h3>${member.name}</h3>
+            <p>${member.address}</p>
+            <p>${member.phone}</p>
+            <a href="${member.website}" target="_blank">Visit Website</a>
+        `;
 
         container.appendChild(card);
-
     });
-
 }
 
 getMembers();
 
-
+// BOTÕES GRID / LIST
 const gridButton = document.querySelector("#grid");
 const listButton = document.querySelector("#list");
 
 gridButton.addEventListener("click", () => {
-
     container.classList.add("grid");
     container.classList.remove("list");
-
 });
 
 listButton.addEventListener("click", () => {
-
     container.classList.add("list");
     container.classList.remove("grid");
+});
 
-})
-
-
-const year = document.querySelector("#currentYear");
-if (year) {
-    year.textContent = new Date().getFullYear();
-}
-
-const lastModified = document.querySelector("#lastModified");
-if (lastModified) {
-    lastModified.textContent = "Last Modified: " + document.lastModified;
-}
+// FOOTER
+document.querySelector("#currentYear").textContent = new Date().getFullYear();
+document.querySelector("#lastModified").textContent = document.lastModified;
