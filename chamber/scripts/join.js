8fcc846c352d8862be27010ae470843db6030ba4
João Victor Oliveira Cavalcante <jvocx.dev@gmail.com>
2026-03-28 09:51:33 -0300

    commit

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,13 +1,30 @@
-// TIMESTAMP
 document.addEventListener("DOMContentLoaded", () => {
+
+    // TIMESTAMP
     document.getElementById("timestamp").value = new Date().toISOString();
+
+    // OPEN MODAL
+    const buttons = document.querySelectorAll(".card");
+    buttons.forEach(btn => {
+        btn.addEventListener("click", () => {
+            const id = btn.dataset.modal;
+            document.getElementById(id).style.display = "block";
+        });
+    });
+
+    // CLOSE MODAL
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach(modal => {
+        modal.addEventListener("click", (e) => {
+            if (e.target === modal) {
+                modal.style.display = "none";
+            }
+        });
+
+        const closeBtn = modal.querySelector("button");
+        closeBtn.addEventListener("click", () => {
+            modal.style.display = "none";
+        });
+    });
+
 });
-
-// MODAL
-function openModal(id) {
-    document.getElementById(id).style.display = "block";
-}
-
-function closeModal(id) {
-    document.getElementById(id).style.display = "none";
-}
